a27dc29f3adadf443e3264bdafabb46030e3900a
myeyesareblind <myeyesareblind@gmail.com>
2015-02-02 23:19:44 +0200

    remove bundle and inline strings

--- Pod/Classes/DMPasscode.m
+++ Pod/Classes/DMPasscode.m
@@ -15,10 +15,6 @@
 #import <LocalAuthentication/LocalAuthentication.h>
 #endif
 
-#undef NSLocalizedString
-#define NSLocalizedString(key, comment) \
-[bundle localizedStringForKey:(key) value:@"" table:@"DMPasscodeLocalisation"]
-
 static DMPasscode* instance;
 static const NSString* KEYCHAIN_NAME = @"passcode";
 static NSBundle* bundle;
@@ -89,7 +85,7 @@ static NSBundle* bundle;
     _completion = completion;
     LAContext* context = [[LAContext alloc] init];
     if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:nil]) {
-        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:NSLocalizedString(@"dmpasscode_touchid_reason", nil) reply:^(BOOL success, NSError* error) {
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:NSLocalizedString(@"Authenticate to access locked feature.", nil) reply:^(BOOL success, NSError* error) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 if (error) {
                     switch (error.code) {
@@ -137,9 +133,9 @@ static NSBundle* bundle;
     DMPasscodeInternalNavigationController* nc = [[DMPasscodeInternalNavigationController alloc] initWithRootViewController:_passcodeViewController];
     [viewController presentViewController:nc animated:YES completion:nil];
     if (_mode == 0) {
-        [_passcodeViewController setInstructions:NSLocalizedString(@"dmpasscode_enter_new_code", nil)];
+        [_passcodeViewController setInstructions:NSLocalizedString(@"Enter new code", nil)];
     } else if (_mode == 1) {
-        [_passcodeViewController setInstructions:NSLocalizedString(@"dmpasscode_enter_to_unlock", nil)];
+        [_passcodeViewController setInstructions:NSLocalizedString(@"Enter code to unlock", nil)];
     }
 }
 
@@ -154,14 +150,14 @@ static NSBundle* bundle;
     if (_mode == 0) {
         if (_count == 0) {
             _prevCode = code;
-            [_passcodeViewController setInstructions:NSLocalizedString(@"dmpasscode_repeat", nil)];
+            [_passcodeViewController setInstructions:NSLocalizedString(@"Repeat code", nil)];
             [_passcodeViewController reset];
         } else if (_count == 1) {
             if ([code isEqualToString:_prevCode]) {
                 [[DMKeychain defaultKeychain] setObject:code forKey:KEYCHAIN_NAME];
                 [self closeAndNotify:YES];
             } else {
-                UIAlertView* errorAlert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"dmpasscode_not_match", nil) message:nil delegate:nil cancelButtonTitle:nil otherButtonTitles:NSLocalizedString(@"dmpasscode_okay", nil), nil];
+                UIAlertView* errorAlert = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Codes did not match, please try again.", nil) message:nil delegate:nil cancelButtonTitle:nil otherButtonTitles:NSLocalizedString(@"Okay", nil), nil];
                 [errorAlert show];
                 [self closeAndNotify:NO];
             }
@@ -170,7 +166,7 @@ static NSBundle* bundle;
         if ([code isEqualToString:[[DMKeychain defaultKeychain] objectForKey:KEYCHAIN_NAME]]) {
             [self closeAndNotify:YES];
         } else {
-            [_passcodeViewController setErrorMessage:[NSString stringWithFormat:NSLocalizedString(@"dmpasscode_n_left", nil), 2 - _count]];
+            [_passcodeViewController setErrorMessage:[NSString stringWithFormat:NSLocalizedString(@"%i attempts left", nil), 2 - _count]];
             [_passcodeViewController reset];
             if (_count >= 2) { // max 3 attempts
                 [self closeAndNotify:NO];
